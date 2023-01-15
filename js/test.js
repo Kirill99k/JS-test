35e1e4b70793cca220a9227e91e952b7c1b9b729
Kirill Kutovuy <114417403+Kirill99k@users.noreply.github.com>
2023-01-15 15:32:57 +0200

    Adds js

---
+++ js/test.js
@@ -0,0 +1,8 @@
+//НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"//
+// const name = 'Kyrylo';
+// const age = 23;
+// const hobby = 'JavaScript';
+// const message = `Good day, my name is ${name}, I am ${age} years old, learning ${hobby}`;
+
+// console.log(message);
+
